05b73cc31c02596d80ffc6c5dfe0c1b090980f2e
李锐 <rayestonal@gmail.com>
2019-11-29 14:13:12 +0800

    es demo

--- spring-boot-mybatis/src/main/java/mobi/rayson/es/ElasticsearchDemo.java
+++ spring-boot-mybatis/src/main/java/mobi/rayson/es/ElasticsearchDemo.java
@@ -2,77 +2,37 @@ package mobi.rayson.es;
 
 import lombok.extern.slf4j.Slf4j;
 import org.apache.http.HttpHost;
-import org.elasticsearch.action.search.SearchRequest;
-import org.elasticsearch.action.search.SearchResponse;
+import org.elasticsearch.action.index.IndexRequest;
+import org.elasticsearch.action.index.IndexResponse;
 import org.elasticsearch.client.RequestOptions;
 import org.elasticsearch.client.RestClient;
 import org.elasticsearch.client.RestHighLevelClient;
-import org.elasticsearch.index.query.BoolQueryBuilder;
-import org.elasticsearch.index.query.QueryBuilders;
-import org.elasticsearch.search.SearchHit;
-import org.elasticsearch.search.builder.SearchSourceBuilder;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 @Slf4j
 @Service
 public class ElasticsearchDemo {
 
-    final private static String[] FETCH_FIELDS = {"@timestamp", "@message"};
-
-    final private static String MATCH_FIELD = "@message";
-    final private static String[] MUST_MATCH = {"brewster-shaw", "AppleWebKit"};
-    final private static String[] MUST_NOT_MATCH = {"21.211.33.63"};
-
-    final private static String TIME_FIELD = "@timestamp";
-    final private static String START_TIME = "2015-05-20T13:06:50";
-    final private static String END_TIME = "2025-05-06T00:00:00";
-
-    final private static String INDEX = "logstash-2015.05.20"; // accepts * as wildcard, .e.g log*
-
     public static void main(String[] args) throws IOException {
 
         RestHighLevelClient client = new RestHighLevelClient(RestClient.builder(new HttpHost("localhost", 9200, "http")));
 
-        SearchSourceBuilder searchSourceBuilder = new SearchSourceBuilder();
 
-        BoolQueryBuilder qb = QueryBuilders.boolQuery();
+        Map<String, Object> jsonMap = new HashMap<>();
+        jsonMap.put("user", "kimchy");
+        jsonMap.put("postDate", new Date());
+        jsonMap.put("message", "trying out Elasticsearch");
+        IndexRequest request = new IndexRequest("posts", "doc", "1")
+                .source(jsonMap);
 
-        for (String match : MUST_MATCH) {
-            qb.must(QueryBuilders.matchQuery(MATCH_FIELD, match));
-        }
-
-        for (String notMatch : MUST_NOT_MATCH) {
-            qb.mustNot(QueryBuilders.matchQuery(MATCH_FIELD, notMatch));
-        }
-
-        qb.must(QueryBuilders.rangeQuery(TIME_FIELD).gte(START_TIME));
-        qb.must(QueryBuilders.rangeQuery(TIME_FIELD).lte(END_TIME));
-
-        searchSourceBuilder.query(qb).fetchSource(FETCH_FIELDS, null);
-
-        SearchRequest searchRequest = new SearchRequest();
-        searchRequest.indices(INDEX);
-        searchRequest.source(searchSourceBuilder);
-
-        SearchResponse searchResponse = client.search(searchRequest, RequestOptions.DEFAULT);
-
-        if (searchResponse.getHits().getTotalHits().value > 0) {
-
-            System.out.println(searchResponse.getHits().getTotalHits());
-
-            for (SearchHit hit : searchResponse.getHits()) {
-                System.out.println("Match: ");
-                for (String fetchField : FETCH_FIELDS) {
-                    System.out.println(" - " + fetchField + " " + hit.getSourceAsMap().get(fetchField));
-                }
-            }
-        } else {
-            System.out.println("No results matching the criteria.");
-        }
+        IndexResponse indexResponse = client.index(request, RequestOptions.DEFAULT);
+        System.out.println(indexResponse);
 
         client.close();
-
     }
 }
