9431b2db17b0dd25e71dda00c02049de03a6c85d
李锐 <rayestonal@gmail.com>
2019-12-02 20:47:09 +0800

    事务。基于2PC

---
+++ rocketmq/src/main/java/com/rayest/transaction/TransactionProducer.java
@@ -0,0 +1,50 @@
+package com.rayest.transaction;
+
+import org.apache.rocketmq.client.exception.MQClientException;
+import org.apache.rocketmq.client.producer.LocalTransactionState;
+import org.apache.rocketmq.client.producer.TransactionListener;
+import org.apache.rocketmq.client.producer.TransactionMQProducer;
+import org.apache.rocketmq.client.producer.TransactionSendResult;
+import org.apache.rocketmq.common.message.Message;
+import org.apache.rocketmq.common.message.MessageExt;
+
+public class TransactionProducer {
+    public static void sendTransaction() throws Exception {
+        TransactionMQProducer producer = new TransactionMQProducer();
+        producer.setNamesrvAddr("127.0.0.1:9876");
+
+        // 设置事务监听器
+        producer.setTransactionListener(new TransactionListener() {
+
+            // 执行 MQ 的本地事务
+            public LocalTransactionState executeLocalTransaction(Message msg, Object arg) {
+                if (msg.getTags().equals("tag1")){
+                    return LocalTransactionState.COMMIT_MESSAGE;
+                }
+                if (msg.getTags().equals("tag2")){
+                    return LocalTransactionState.ROLLBACK_MESSAGE;
+                }
+                if (msg.getTags().equals("tag3")){
+                    return LocalTransactionState.UNKNOW;
+                }
+                return LocalTransactionState.UNKNOW;
+            }
+
+            // MQ 回查本地事务状态
+            public LocalTransactionState checkLocalTransaction(MessageExt msg) {
+                System.out.println("消息tag：" + msg.getTags());
+                return LocalTransactionState.COMMIT_MESSAGE;
+            }
+        });
+        producer.start();
+
+        for (int i = 0; i < 10; i++) {
+            Message message = new Message("transaction-topic", "tag1", ("Hello:" + i).getBytes());
+            TransactionSendResult result = producer.sendMessageInTransaction(message, null);
+            System.out.println("发送结果:" + result);
+            Thread.sleep(1000);
+        }
+        producer.shutdown();
+
+    }
+}
