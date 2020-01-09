ddbc53d0a1ca0ab048781078231d3f78a551c57b
李锐 <rayestonal@gmail.com>
2020-01-09 19:55:48 +0800

    redis

--- architecture-java/src/test/java/com.rayest/RedisTest.java
+++ architecture-java/src/test/java/com.rayest/RedisTest.java
@@ -2,7 +2,9 @@ package com.rayest;
 
 import com.rayest.common.Note;
 import org.junit.Test;
+import org.springframework.data.redis.core.Cursor;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ScanOptions;
 import org.springframework.data.redis.core.SetOperations;
 import org.springframework.data.redis.core.ZSetOperations;
 
@@ -18,16 +20,16 @@ import static org.junit.Assert.assertEquals;
 public class RedisTest extends BaseTest {
 
     @Resource
-    private RedisTemplate<String, String> stringStringRedisTemplate;
+    private RedisTemplate<String, String> stringRedisTemplate;
 
     @Test
     public void test_string() {
         String key = "name-string";
         String value = "lee";
-        stringStringRedisTemplate.opsForValue().set(key, value);
-        stringStringRedisTemplate.expire(key, 60, TimeUnit.SECONDS);
+        stringRedisTemplate.opsForValue().set(key, value);
+        stringRedisTemplate.expire(key, 60, TimeUnit.SECONDS);
 
-        String name = stringStringRedisTemplate.opsForValue().get(key);
+        String name = stringRedisTemplate.opsForValue().get(key);
 
         assertEquals("lee", name);
     }
@@ -37,9 +39,9 @@ public class RedisTest extends BaseTest {
     public void test_right_list() {
         String key = "colors";
         List<String> colors = Arrays.asList("RED", "BLACK", "BLUE");
-        stringStringRedisTemplate.opsForList().rightPushAll(key, colors);
-        stringStringRedisTemplate.expire(key, 60, TimeUnit.SECONDS);
-        String red = stringStringRedisTemplate.opsForList().leftPop(key);
+        stringRedisTemplate.opsForList().rightPushAll(key, colors);
+        stringRedisTemplate.expire(key, 60, TimeUnit.SECONDS);
+        String red = stringRedisTemplate.opsForList().leftPop(key);
 
         assertEquals("RED", red);
     }
@@ -47,8 +49,8 @@ public class RedisTest extends BaseTest {
     @Test
     public void test_set() {
         String key = "users";
-        stringStringRedisTemplate.opsForSet().add(key, "1", "2", "3");
-        SetOperations<String, String> set = stringStringRedisTemplate.opsForSet();
+        stringRedisTemplate.opsForSet().add(key, "1", "2", "3");
+        SetOperations<String, String> set = stringRedisTemplate.opsForSet();
 
         Long size = set.size(key);
         assert size != null;
@@ -64,9 +66,9 @@ public class RedisTest extends BaseTest {
         users.put("name", "lee");
         users.put("age", "20");
         users.put("city", "shanghai");
-        stringStringRedisTemplate.opsForHash().putAll(key, users);
+        stringRedisTemplate.opsForHash().putAll(key, users);
 
-        Map<Object, Object> map = stringStringRedisTemplate.opsForHash().entries(key);
+        Map<Object, Object> map = stringRedisTemplate.opsForHash().entries(key);
         assertEquals("lee", map.get("name"));
     }
 
@@ -74,15 +76,33 @@ public class RedisTest extends BaseTest {
     @Note("缓存三个用户的粉丝数量")
     public void test_z_set() {
         String key = "fans";
-        stringStringRedisTemplate.opsForZSet().add(key, "Lee", 1000);
-        stringStringRedisTemplate.opsForZSet().add(key, "Adel", 2000);
-        stringStringRedisTemplate.opsForZSet().add(key, "Zoe", 3000);
+        stringRedisTemplate.opsForZSet().add(key, "Lee", 1000);
+        stringRedisTemplate.opsForZSet().add(key, "Adel", 2000);
+        stringRedisTemplate.opsForZSet().add(key, "Zoe", 3000);
 
-        ZSetOperations<String, String> zSet = stringStringRedisTemplate.opsForZSet();
+        ZSetOperations<String, String> zSet = stringRedisTemplate.opsForZSet();
         assertEquals(3, zSet.size(key).intValue());
         assertEquals(1000, zSet.score(key, "Lee").intValue());
         assertEquals(0, zSet.rank(key, "Lee").intValue());
         assertEquals(1, zSet.rank(key, "Adel").intValue());
         assertEquals(2, zSet.rank(key, "Zoe").intValue());
+    }
+
+    @Test
+    @Note("scan(): 过滤集合中的元素")
+
+    
+    public void test_scan() {
+        String key = "scan-key";
+        stringRedisTemplate.opsForSet().add(key, "key1001", "key1002", "key2000");
+        ScanOptions scanOptions = ScanOptions.scanOptions().match("key1*").count(3).build();
+        Cursor<String> cursor = stringRedisTemplate.opsForSet().scan(key, scanOptions);
+        int count = 0;
+        while (cursor.hasNext()){
+            String value = cursor.next();
+            System.out.println(value);
+            count++;
+        }
+        assertEquals(2, count);
     }
 }
