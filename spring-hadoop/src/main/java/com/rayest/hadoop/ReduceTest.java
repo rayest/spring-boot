bc21deb045878c882f75d574c1ae3bd050a39d5e
李锐 <rayestonal@gmail.com>
2020-02-12 00:30:49 +0800

    修改方法错误

--- spring-hadoop/src/main/java/com/rayest/hadoop/ReduceTest.java
+++ spring-hadoop/src/main/java/com/rayest/hadoop/ReduceTest.java
@@ -9,10 +9,11 @@ import java.util.Iterator;
 
 public class ReduceTest extends Reducer<Text, IntWritable, Text, IntWritable> {
 
-    public void reduce(Text key, Iterator<IntWritable> values, Context context) throws IOException, InterruptedException {
-        System.out.println("reduce context:" + context);
+    @Override
+    public void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
+        System.out.println("reduce key:" + key);
         int sum = 0;
-        Iterator<IntWritable> it = values;
+        Iterator<IntWritable> it = values.iterator();
         while (it.hasNext()) {
             IntWritable val = it.next();
             sum += val.get();
